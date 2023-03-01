45332f431ce70366911411af53c94153a7bdfd77
talitha0911 <114551398+talitha0911@users.noreply.github.com>
2023-03-01 23:11:58 +0700

    Add files via upload

---
+++ praktikum1/coba1.php
@@ -0,0 +1,9 @@
+<?php
+ 
+    define('PHI', 3.14 );
+    define('DBNAME', 'inventori');
+    define('DBSERVER', 'localhost');
+
+    echo 'Nilai phi : ' . PHI;
+    echo '<br> Nama database nya : ' . DBNAME;
+    echo '<br> Lokasi Database nya : ' . DBSERVER;
---
+++ praktikum1/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $nama = 'talitha';
+    $umur = 18;
+    $berat = 58;
+
+    echo 'nama saya : ' . $nama;
+    echo '<br>umur saya : ' . $umur;
+    echo '<br>berat saya : ' . $berat;
+
